d1599883f42b4dbc30b8695b031776e4d5cf53b4
N8tivegamer <gamerboz652@gmail.com>
2025-12-08 14:17:27 -0600

    Initial commit

---
+++ Greetings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes_Hill_Adam
+{
+    internal class Greetings
+    {
+
+        public void Welcome()
+        {
+            Console.WriteLine("Hello Travler, welcome");
+        }
+
+        public void Hello(string name)
+        {
+            Console.WriteLine($"{name}, Thank you for joining us today!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace classes_Hill_Adam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Greetings greetings = new Greetings();
+            greetings.Welcome();
+
+            Console.WriteLine("What is your name squire?");
+            string userName = Console.ReadLine();
+
+            greetings.Hello(userName);
+
+        }
+    }
+}
